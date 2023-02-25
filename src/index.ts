324a39a855535d4721dd69728bfef044bf666fb7
geoffreytools <29453961+geoffreytools@users.noreply.github.com>
2023-02-25 19:57:36 +0100

    feat: expose Next and Prev

--- src/index.ts
+++ src/index.ts
@@ -9,5 +9,5 @@ export * from './unwrap';
 export * from './Replace';
 export * from './hkt';
 export * as free from './free-types';
-export { Slice, ToTuple, Head, Tail, Last, Init, IsUnknown, IsOptional } from './utils'
+export { Slice, ToTuple, Head, Tail, Last, Init, IsUnknown, IsOptional, Next, Prev } from './utils'
 export { TypesMap } from './TypesMap';
